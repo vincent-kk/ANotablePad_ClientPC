aa6e8e4da1e2f578964f11fcfbc13e15be080be4
KLumy <lunox273@gmail.com>
2019-11-24 20:12:54 +0900

    Connect With Server

---
+++ Assets/Script/ApplicationManager.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using FreeDraw;
+using UnityEngine;
+
+public class ApplicationManager : MonoBehaviour
+{
+    [SerializeField] private GameObject[] views;
+    [SerializeField] private Drawable _drawable;
+    [SerializeField] private DrawingSettings _drawingSettings;
+    [SerializeField] private NetworkManager _networkManager;
+
+    private readonly Dictionary<string, int> _viewName = new Dictionary<string, int>(4)
+    {
+        {"connection", 0},
+        {"menu", 1},
+        {"room", 2},
+        {"draw", 3}
+    };
+
+    private void Start()
+    {
+        ChangeView("connection");
+    }
+
+    public void ChangeView(string view)
+    {
+        var target = _viewName[view];
+        for (var i = 0; i < views.Length; i++)
+            views[i].SetActive(i == target);
+
+        StartDrawing(target == 3);
+    }
+
+    private void StartDrawing(bool start)
+    {
+        _drawable.SetNewDrawing(start);
+    }
+
+    public void ChangeColor(string color)
+    {
+    }
+
+    public void SendCoordinateData(Vector2 data)
+    {
+        _networkManager.Send(data.ToString());
+    }
+
+    public Vector2 ReceiveCoordinateData()
+    {
+        var msg = _networkManager.Receive();
+        if (msg == null) return new Vector2();
+//        if (msg.Contains("@"))
+//        {
+//
+//        }
+//        else
+//        {
+//
+//        }
+
+        Debug.Log(msg);
+        return new Vector2();
+    }
+}
---
+++ Assets/Script/NetworkManager.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.IO;
+using System.Net.Sockets;
+using System.Text;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NetworkManager : MonoBehaviour
+{
+    [SerializeField] private InputField serverIp = null;
+    [SerializeField] private InputField serverPort = null;
+    [SerializeField] private ApplicationManager app;
+
+    public int bufferSize = 1024;
+    private readonly string _delimiter = "|";
+
+
+    private byte[] _receiveBuffer;
+
+    private TcpManager _tcpManager = null;
+    private Encoding _encode;
+
+    private static ChatState _state = ChatState.HOST_TYPE_SELECT;
+
+    enum ChatState
+    {
+        HOST_TYPE_SELECT = 0, // 방 선택.
+        CONNECTION, // 연결.
+        LEAVE, // 나가기.
+        ERROR, // 오류.
+    };
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _tcpManager = gameObject.AddComponent<TcpManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch (_state)
+        {
+            case ChatState.HOST_TYPE_SELECT:
+                break;
+            case ChatState.CONNECTION:
+                Receive();
+                break;
+            case ChatState.LEAVE:
+                break;
+        }
+    }
+
+    public void ConnectToServer()
+    {
+        var ip = serverIp.text;
+        var port = Convert.ToInt32(serverPort.text);
+
+        Debug.Log("server : " + ip + " : " + port);
+
+        serverIp.text = "";
+        serverPort.text = "";
+
+        if (TcpConnection(ip, port))
+        {
+            Send("@Host-PC");
+            app.ChangeView("draw");
+        }
+        else
+        {
+            ConsoleLogger("Fail To Connect Server");
+        }
+    }
+
+    private bool TcpConnection(string serverIp, int port)
+    {
+        var ret = _tcpManager.Connect(serverIp, port);
+        _state = ret ? ChatState.CONNECTION : ChatState.ERROR;
+        return ret;
+    }
+
+    public void Send(string msg)
+    {
+        msg += _delimiter;
+        var buffer = System.Text.Encoding.UTF8.GetBytes(msg);
+        _tcpManager.Send(buffer, buffer.Length);
+        ConsoleLogger(msg);
+    }
+
+    public string Receive()
+    {
+        var returnData = new byte[bufferSize];
+        var recvSize = _tcpManager.Receive(ref returnData, returnData.Length);
+        if (recvSize > 0)
+        {
+            var msg = System.Text.Encoding.UTF8.GetString(returnData);
+            ConsoleLogger(msg);
+            return msg;
+        }
+        return null;
+    }
+
+    private void ConsoleLogger(string log)
+    {
+        Debug.Log(log);
+    }
+
+    private void OnApplicationQuit()
+    {
+        if (_tcpManager != null)
+        {
+            _tcpManager.StopServer();
+        }
+    }
+
+    public void OnEventHandling(NetEventState state)
+    {
+        switch (state.type)
+        {
+            case NetEventType.Connect:
+                if (_tcpManager.IsServer())
+                {
+                }
+                else
+                {
+                }
+
+                break;
+
+            case NetEventType.Disconnect:
+                if (_tcpManager.IsServer())
+                {
+                }
+                else
+                {
+                }
+
+                break;
+        }
+    }
+}
